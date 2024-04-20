86203a54d502366e859ac2a9a3cb193f49c38536
Needrandom <adrian.holzbeck@gmail.com>
2024-04-20 17:16:41 +0200

    letter count function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book = "books/frankenstein.txt"
     contents = get_book(book)
     num_words = get_wordcount(contents)
-    print(num_words)
+    letters = letter_count(contents)
+    print(letters)
 
 def get_book(book):
     with open(book) as f:
@@ -12,8 +13,16 @@ def get_wordcount(contents):
     wordcount = contents.split()
     return len(wordcount)
 
-def leter_count(contents):
+def letter_count(contents):
     lowercase = contents.lower()
+    lower_list =list(lowercase)
+    letter_dict = {}
+    for i in lower_list:
+        if i in letter_dict:
+            letter_dict[i] += 1
+        elif i.isalpha():
+            letter_dict[i] = 1
+    return letter_dict
 
 
 
